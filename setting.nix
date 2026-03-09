9a8f09959feb5b4c013e5b94453e74fc7e578918
weifeng <849343517@qq.com>
2026-03-10 01:01:14 +0800

    add dock setting

--- setting.nix
+++ setting.nix
@@ -7,5 +7,14 @@
     primaryUser = "user";
     configurationRevision = self.rev or self.dirtyRev or null;
     stateVersion = 6;
+
+    defaults = {
+      dock = {
+        autohide = true;
+        largesize = 110;
+        tilesize = 45;
+        magnification = true;
+      };
+    };
   };
 }
